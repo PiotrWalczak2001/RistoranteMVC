d8a1763ca640875dd71dca54c1e07edd383c00e4
PiotrWalczak2001 <01.piotrwalczak@gmail.com>
2021-10-13 23:43:27 +0200

    removing virtual from models

--- RistoranteMVC/RistoranteMVC/Models/Dish.cs
+++ RistoranteMVC/RistoranteMVC/Models/Dish.cs
@@ -10,7 +10,7 @@ namespace RistoranteMVC.Models
         public Guid DishId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public virtual Category Category { get; set; }
+        public Category Category { get; set; }
         public Guid CategoryId { get; set; }
         public int Kcal { get; set; }
         public decimal Price { get; set; }
--- RistoranteMVC/RistoranteMVC/Models/OrderDetail.cs
+++ RistoranteMVC/RistoranteMVC/Models/OrderDetail.cs
@@ -12,7 +12,7 @@ namespace RistoranteMVC.Models
         public Guid DishId { get; set; }
         public int Amount { get; set; }
         public decimal Price { get; set; }
-        public virtual Dish Dish { get; set; }
-        public virtual Order Order { get; set; }
+        public Dish Dish { get; set; }
+        public Order Order { get; set; }
     }
 }
